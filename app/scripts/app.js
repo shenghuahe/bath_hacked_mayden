fc026eed0b81697fc385df80eeb514a6d377eb11
Tom <tomjharding@live.co.uk>
2014-03-22 10:43:24 +0000

    Setting up the Yeoman business
    yo angular-fullstack

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('hackedApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+      
+    $locationProvider.html5Mode(true);
+  });
